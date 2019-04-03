e2e307dca03cdd20459ac2462c72461457e10336
Chris Valenzuela <chriss.valenzuela@yahoo.com>
2019-04-02 18:36:14 -0700

    adds the articles according to number selected.. needs work on start/end year but that is if statements. maybe research params() could help

--- nyt.js
+++ nyt.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 var clear = function() {
     $("#article-section").empty();
-    document.getElementById('keyword').value = "";
+    
 }
 
 
@@ -21,16 +21,38 @@ $("#search-button").on("click", function(event){
     var searched = $("#keyword").val();
 
     console.log(searched);
-    for ( var i = 0; i < numIntArticles; i++){
-        var articleDiv = $("<div>");
-        var p = $("<p>").text("testing");
-        articleDiv.append(p);
-        $("#article-section").append(articleDiv);
-    }
 
+
+    var APIkey = "gKG7mZSCUAzBbtpMtwg1PZT5GJubSsCA"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searched + "&api-key=" + APIkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        var article = response.response;
+        console.log(article.docs[0]);
+
+        for ( var i = 0; i < numIntArticles; i++){
+
+            var articleDiv = $("<div>");
+            var list = $("<ul>");
+            list.addClass("list-group");
+            var p = $("<li>").text((i + 1) + " " + article.docs[i].headline.main).addClass("list-group-item");
+            var author = p.append("<br>" + article.docs[i].byline.original).append("<br> Section Name: " + article.docs[i].section_name);
+            var date = author.append("<br>" + article.docs[i].pub_date);
+            var webURL = date.append("<br>" + "<a href='" + article.docs[i].web_url + "'>" + article.docs[i].web_url + "</a>");
+            
+            articleDiv.append(list).append(author).append(date).append(webURL);
+
+            
+            $("#article-section").append(articleDiv);
+        }
+        
+
+    });
   
 });
-
 
 
 });
